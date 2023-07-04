4c5e508857321e4677995bc4e518c40e36125069
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-05 00:40:39 +0300

    Первый

---
+++ src/main/java/weblib/hw2_4/Hw24Application.java
@@ -0,0 +1,13 @@
+package weblib.hw2_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw24Application.class, args);
+    }
+
+}
